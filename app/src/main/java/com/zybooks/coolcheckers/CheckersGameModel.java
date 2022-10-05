1f053021c924a021fe3b8cff58ce1a6a88eab735
AndrewTheCoder <76978271+TheFadedOne@users.noreply.github.com>
2022-10-04 21:37:26 -0400

    Editing CheckersGameModel
    working on setting up the model for the checkers game

---
+++ app/src/main/java/com/zybooks/coolcheckers/CheckersGameModel.java
@@ -0,0 +1,41 @@
+package com.zybooks.coolcheckers;
+
+
+public class CheckersGameModel {
+
+    public enum checkers {PLAYER1, PLAYER2, PLAYER1_KING, PLAYER2_KING};
+    public enum spaceType {WHITE_SPACE, BLACK_SPACE};
+    public spaceType[][] mGameBoard;
+    public checkers[][] mCheckerPositions;
+    public final int BOARD_SIZE = 8;
+
+
+    //constructor
+    public CheckersGameModel()
+    {
+        mGameBoard = new spaceType[BOARD_SIZE][BOARD_SIZE];
+        mCheckerPositions = new checkers[BOARD_SIZE][BOARD_SIZE];
+    }
+
+
+    //sets up all of the white and black spaces on the checker board
+    public void setUpGameBoard()
+    {
+        int count = 0;
+        for (int i = 0; i < BOARD_SIZE; ++i)
+        {
+            for (int j = 0; j < BOARD_SIZE; ++j)
+            {
+                mGameBoard[i][j] = (count % 2 == 1) ? spaceType.WHITE_SPACE : spaceType.BLACK_SPACE;
+                count++;
+            }
+        }
+    }
+
+
+    //sets up the default positions of the player pieces
+    public void setUpCheckerPositions()
+    {
+
+    }
+}
