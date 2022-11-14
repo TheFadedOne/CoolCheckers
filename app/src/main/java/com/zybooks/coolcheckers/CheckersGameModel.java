19c081b245b5ed6afb0ce2b59ddd3046e2833096
AndrewTheCoder <76978271+TheFadedOne@users.noreply.github.com>
2022-11-13 21:01:54 -0500

    Testing out how we may use a single array to contain all pieces

--- app/src/main/java/com/zybooks/coolcheckers/CheckersGameModel.java
+++ app/src/main/java/com/zybooks/coolcheckers/CheckersGameModel.java
@@ -3,25 +3,42 @@ package com.zybooks.coolcheckers;
 
 public class CheckersGameModel {
 
+<<<<<<< Updated upstream
 
     public enum spaceType {WHITE_SPACE, BLACK_SPACE};
     public spaceType[][] mGameBoard;
     public checkers[][] mGamePieces;
     public static final int BOARD_SIZE = 8;
+=======
+    public GamePiece[] black_pieces;
+    public GamePiece[] red_pieces;
+    
+    public GamePiece[] mGamePieces;
+    public static final int MOVEABLE_SPACES = 32;
+>>>>>>> Stashed changes
 
 
     //constructor
     public CheckersGameModel()
     {
+<<<<<<< Updated upstream
         mGameBoard = new spaceType[BOARD_SIZE][BOARD_SIZE];
         checkers = new ;
+=======
+        mGameBoard = new spaceType[MOVEABLE_SPACES];
+>>>>>>> Stashed changes
     }
 
 
     public void newGame()
     {
+<<<<<<< Updated upstream
         setUpGameBoard();
         setUpCheckerPositions();
+=======
+       setUpGameBoard();
+       setUpPiecePositions();
+>>>>>>> Stashed changes
     }
 
 
@@ -50,4 +67,15 @@ public class CheckersGameModel {
 
         }
     }
+
+    /*
+    takes input of the player's selected piece position to move and desired position to move it to.
+    It will check adjacent positions and see if there is an empty space or other piece.
+     */
+    public boolean checkValidMove()
+    {
+        return false;
+    }
+
 }
+
