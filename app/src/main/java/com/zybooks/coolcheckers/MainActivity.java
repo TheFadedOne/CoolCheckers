9fc9ef3d77d6b514a1da5f6b53710aa34ace42e6
Pillows0 <116773623+Pillows0@users.noreply.github.com>
2022-12-07 18:36:35 -0500

    Merge branch 'CoolBranch' of /Users/iradianne/Documents/GitHub/CoolCheckers with conflicts.

--- app/src/main/java/com/zybooks/coolcheckers/MainActivity.java
+++ app/src/main/java/com/zybooks/coolcheckers/MainActivity.java
@@ -2,6 +2,7 @@ package com.zybooks.coolcheckers;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -9,14 +10,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -24,7 +23,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
 
     public Menu mMenu;
 
@@ -49,44 +47,5 @@ public class MainActivity extends AppCompatActivity {
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
             NavigationUI.setupWithNavController(navView, navController);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.board_menu, menu);
-        mMenu = menu;
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        View gameBoard = findViewById(R.id.board);
-        Drawable standard = getResources().getDrawable(R.drawable.checkerboard);
-        Drawable green = getResources().getDrawable(R.drawable.checkerboardgreen);
-        Drawable ice = getResources().getDrawable(R.drawable.checkerboardice);
-        Drawable ruby = getResources().getDrawable(R.drawable.checkerboardruby);
-
-
-        // Determine which menu option was chosen
-        if (item.getItemId() == R.id.board_stardard) {
-
-            return true;
-        }
-        else if (item.getItemId() == R.id.board_green) {
-            gameBoard.setBackground(green);
-            return true;
-        }
-        else if (item.getItemId() == R.id.board_ice) {
-            gameBoard.setBackground(ice);
-            return true;
-        }
-        else if (item.getItemId() == R.id.board_ruby) {
-            gameBoard.setBackground(ruby);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
